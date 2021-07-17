adeb263fd9b83f8d2fca55806f2b47f3e5498d99
ap-coder <apcoder2019@gmail.com>
2021-07-16 18:17:08 -0600

    Update OutgoingController.php

--- app/Http/Controllers/OutgoingController.php
+++ app/Http/Controllers/OutgoingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Mail\SendGrid;
+use App\Mail\TestEmail;
 
  
 class OutgoingController extends Controller
@@ -14,5 +15,12 @@ class OutgoingController extends Controller
         $input = ['message' => 'This is a test!'];
 
         Mail::to('phillip.madsen.12@gmail.com')->send(new SendGrid($input));
+    }
+
+    public function testMail(Requset $request)
+    {
+        $data = ['message' => 'This is a test!'];
+
+        Mail::to('john@example.com')->send(new TestEmail($data));
     }
 }
