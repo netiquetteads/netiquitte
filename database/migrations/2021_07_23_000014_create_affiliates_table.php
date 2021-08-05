feb87c2b3d0319ee5a7350fb28a8f5b22a05ada8
ap-coder <apcoder2019@gmail.com>
2021-08-04 19:14:33 -0600

    Revert "changes"
    This reverts commit 7bcf6739515173925d111a2cb8547428f4cd19ce.

--- database/migrations/2021_07_23_000014_create_affiliates_table.php
+++ database/migrations/2021_07_23_000014_create_affiliates_table.php
@@ -11,19 +11,22 @@ class CreateAffiliatesTable extends Migration
         Schema::create('affiliates', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name')->nullable();
+            $table->string('account_status')->nullable();
             $table->string('everflow_account')->nullable();
             $table->string('account_manager_name')->nullable();
             $table->string('account_executive_name')->nullable();
             $table->float('balance', 15, 2)->nullable();
-            $table->datetime('last_login')->nullable();
+            $table->unsignedBigInteger('last_login')->nullable();
             $table->string('network_country_code')->nullable();
             $table->string('global_tracking_domain_url')->nullable();
             $table->boolean('published')->default(0)->nullable();
-            $table->decimal('today_revenue', 15, 2)->nullable();
+            $table->string('today_revenue')->nullable();
             $table->integer('network_affiliateid')->nullable();
             $table->integer('account_executiveid')->nullable();
             $table->integer('account_managerid')->nullable();
             $table->integer('networkid')->nullable();
+            $table->unsignedBigInteger('time_created')->nullable();
+            $table->unsignedBigInteger('time_saved')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
