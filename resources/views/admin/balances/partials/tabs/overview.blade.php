ea6a6675ff9f418c1d9de6f2bebe51ec31f0c2c0
ap-coder <apcoder2019@gmail.com>
2021-08-25 17:35:03 -0600

    redo modal

---
+++ resources/views/admin/balances/partials/tabs/overview.blade.php
@@ -0,0 +1,57 @@
+                  <div class="tab-pane text-left fade show active" id="vert-tabs-overview" role="tabpanel" aria-labelledby="vert-tabs-overview-tab">
+				        <h5 class="mb-2">Info Box With Custom Shadows <small><i>Using Bootstrap's Shadow Utility</i></small></h5>
+				        <div class="row">
+				          <div class="col-md-3 col-sm-6 col-12">
+				            <div class="info-box shadow-none">
+				              <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+
+				              <div class="info-box-content">
+				                <span class="info-box-text">Shadows</span>
+				                <span class="info-box-number">None</span>
+				              </div>
+				              <!-- /.info-box-content -->
+				            </div>
+				            <!-- /.info-box -->
+				          </div>
+				          <!-- /.col -->
+				          <div class="col-md-3 col-sm-6 col-12">
+				            <div class="info-box shadow-sm">
+				              <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+
+				              <div class="info-box-content">
+				                <span class="info-box-text">Shadows</span>
+				                <span class="info-box-number">Small</span>
+				              </div>
+				              <!-- /.info-box-content -->
+				            </div>
+				            <!-- /.info-box -->
+				          </div>
+				          <!-- /.col -->
+				          <div class="col-md-3 col-sm-6 col-12">
+				            <div class="info-box shadow">
+				              <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+
+				              <div class="info-box-content">
+				                <span class="info-box-text">Shadows</span>
+				                <span class="info-box-number">Regular</span>
+				              </div>
+				              <!-- /.info-box-content -->
+				            </div>
+				            <!-- /.info-box -->
+				          </div>
+				          <!-- /.col -->
+				          <div class="col-md-3 col-sm-6 col-12">
+				            <div class="info-box shadow-lg">
+				              <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
+
+				              <div class="info-box-content">
+				                <span class="info-box-text">Shadows</span>
+				                <span class="info-box-number">Large</span>
+				              </div>
+				              <!-- /.info-box-content -->
+				            </div>
+				            <!-- /.info-box -->
+				          </div>
+				          <!-- /.col -->
+				        </div>
+                  </div>
