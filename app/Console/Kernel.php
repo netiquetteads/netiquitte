42efc383e7117a45a5fdbb2d013c89681ee54e56
Tapas vishwas <tpsvishwas78@gmail.com>
2021-12-28 02:27:58 +0530

    backup setup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,21 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->daily()->at('01:30')
+        ->onFailure(function () {
+            \Log::info('backup clean failed');
+         })
+         ->onSuccess(function () {
+            \Log::info('backup clean successfull');
+         });
+
+        $schedule->command('backup:run --only-db')->daily()->at('02:00')
+        ->onFailure(function () {
+            \Log::info('backup failed');
+         })
+         ->onSuccess(function () {
+            \Log::info('backup successfull');
+         });
     }
 
     /**
