1019c832d421b5dbd369f2839846997891023a67
Tapas vishwas <tpsvishwas78@gmail.com>
2022-01-17 17:56:57 +0530

    change datatable

--- resources/views/admin/campaigns/index.blade.php
+++ resources/views/admin/campaigns/index.blade.php
@@ -24,18 +24,19 @@
     </div>
 
     <div class="card-body">
-        <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-Campaign">
+        <table class="table table-bordered table-striped table-hover ajaxTable datatable datatable-Campaign">
             <thead>
                 <tr>
+                    <th></th>
                   <th width="10">
                     <input type="checkbox" name="selectall" id="selectall">
                 </th>
                     <th>
                         {{ trans('cruds.campaign.fields.id') }}
                     </th>
-                    <th>
+                    {{-- <th>
                         {{ trans('cruds.campaign.fields.name') }}
-                    </th>
+                    </th> --}}
                     <th>
                         {{ trans('cruds.campaign.fields.email_subject') }}
                     </th>
@@ -45,9 +46,9 @@
                     <th>
                         {{ trans('cruds.campaign.fields.sentDateTime') }}
                     </th>
-                    <th>
+                    {{-- <th>
                         {{ trans('cruds.campaign.fields.sentTo') }}
-                    </th>
+                    </th> --}}
                     {{-- <th>
                         {{ trans('cruds.campaign.fields.subs') }}
                     </th>
@@ -69,73 +70,192 @@
 @section('scripts')
 @parent
 <script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('campaign_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.campaigns.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).data(), function (entry) {
-          return entry.id
-      });
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
+    //
+// Pipelining function for DataTables. To be used to the `ajax` option of DataTables
+//
+$.fn.dataTable.pipeline = function ( opts ) {
+    // Configuration options
+    var conf = $.extend( {
+        pages: 5,     // number of pages to cache
+        url: '',      // script url
+        data: null,   // function or object with parameters to send to the server
+                      // matching how `ajax.data` works in DataTables
+        method: 'GET' // Ajax HTTP method
+    }, opts );
+ 
+    // Private variables for storing the cache
+    var cacheLower = -1;
+    var cacheUpper = null;
+    var cacheLastRequest = null;
+    var cacheLastJson = null;
+ 
+    return function ( request, drawCallback, settings ) {
+        var ajax          = false;
+        var requestStart  = request.start;
+        var drawStart     = request.start;
+        var requestLength = request.length;
+        var requestEnd    = requestStart + requestLength;
+         
+        if ( settings.clearCache ) {
+            // API requested that the cache be cleared
+            ajax = true;
+            settings.clearCache = false;
+        }
+        else if ( cacheLower < 0 || requestStart < cacheLower || requestEnd > cacheUpper ) {
+            // outside cached data - need to make a request
+            ajax = true;
+        }
+        else if ( JSON.stringify( request.order )   !== JSON.stringify( cacheLastRequest.order ) ||
+                  JSON.stringify( request.columns ) !== JSON.stringify( cacheLastRequest.columns ) ||
+                  JSON.stringify( request.search )  !== JSON.stringify( cacheLastRequest.search )
+        ) {
+            // properties changed (ordering, columns, searching)
+            ajax = true;
+        }
+         
+        // Store the request for checking next time around
+        cacheLastRequest = $.extend( true, {}, request );
+ 
+        if ( ajax ) {
+            // Need data from the server
+            if ( requestStart < cacheLower ) {
+                requestStart = requestStart - (requestLength*(conf.pages-1));
+ 
+                if ( requestStart < 0 ) {
+                    requestStart = 0;
+                }
+            }
+             
+            cacheLower = requestStart;
+            cacheUpper = requestStart + (requestLength * conf.pages);
+ 
+            request.start = requestStart;
+            request.length = requestLength*conf.pages;
+ 
+            // Provide the same `data` options as DataTables.
+            if ( typeof conf.data === 'function' ) {
+                // As a function it is executed with the data object as an arg
+                // for manipulation. If an object is returned, it is used as the
+                // data object to submit
+                var d = conf.data( request );
+                if ( d ) {
+                    $.extend( request, d );
+                }
+            }
+            else if ( $.isPlainObject( conf.data ) ) {
+                // As an object, the data given extends the default
+                $.extend( request, conf.data );
+            }
+ 
+            return $.ajax( {
+                "type":     conf.method,
+                "url":      conf.url,
+                "data":     request,
+                "dataType": "json",
+                "cache":    false,
+                "success":  function ( json ) {
+                    cacheLastJson = $.extend(true, {}, json);
+ 
+                    if ( cacheLower != drawStart ) {
+                        json.data.splice( 0, drawStart-cacheLower );
+                    }
+                    if ( requestLength >= -1 ) {
+                        json.data.splice( requestLength, json.data.length );
+                    }
+                     
+                    drawCallback( json );
+                }
+            } );
+        }
+        else {
+            json = $.extend( true, {}, cacheLastJson );
+            json.draw = request.draw; // Update the echo for each response
+            json.data.splice( 0, requestStart-cacheLower );
+            json.data.splice( requestLength, json.data.length );
+ 
+            drawCallback(json);
+        }
     }
-  }
-  dtButtons.push(deleteButton)
-@endcan
+};
+ 
+// Register an API method that will empty the pipelined data, forcing an Ajax
+// fetch on the next draw (i.e. `table.clearPipeline().draw()`)
+$.fn.dataTable.Api.register( 'clearPipeline()', function () {
+    return this.iterator( 'table', function ( settings ) {
+        settings.clearCache = true;
+    } );
+} );
 
-  let dtOverrideGlobals = {
-    buttons: dtButtons,
-    processing: true,
-    serverSide: true,
-    retrieve: true,
-    aaSorting: [],
-    ajax: "{{ route('admin.campaigns.index') }}",
-    columns: [
-      { data: 'placeholder', name: 'placeholder' },
-{ data: 'id', name: 'id' },
-{ data: 'name', name: 'name' },
-{ data: 'email_subject', name: 'email_subject' },
-// { data: 'campaign_offer_name', name: 'campaign_offer.name' },
-{ data: 'sentDateTime', name: 'sentDateTime' },
-{ data: 'send_to', name: 'send_to' },
-// { data: 'subs', name: 'subs' },
-// { data: 'opens', name: 'opens' },
-{ data: 'actions', name: '{{ trans('global.actions') }}' }
-    ],
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-    "createdRow": function( row, data, dataIndex ) {
 
-// Add a class to the cell in the second column
-$(row).children(':nth-child(7)').addClass('text-center');
+   /* Formatting function for row details - modify as you need */
+function format ( d ) {
+    console.log('tabeldata',d);
+    // `d` is the original data object for the row
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+        '<tr>'+
+            '<td>{{ trans('cruds.campaign.fields.name') }}:</td>'+
+            '<td>'+d.name+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>{{ trans('cruds.campaign.fields.sentTo') }}:</td>'+
+            '<td>'+d.send_to+'</td>'+
+        '</tr>'+
+        '<tr>'+
+            '<td>{{ trans('cruds.campaign.fields.sentDateTime') }}:</td>'+
+            '<td>'+d.sentDateTime+'</td>'+
+        '</tr>'+
+    '</table>';
 }
-  };
-  let table = $('.datatable-Campaign').DataTable(dtOverrideGlobals);
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-});
+ 
+$(document).ready(function() {
+    // Add event listener for opening and closing details
+    $('.datatable-Campaign').on('click', 'tbody td.dt-control', function () {
+        var tr = $(this).closest('tr');
+        console.log('tr',tr);
+        var row = table.row( tr );
+ 
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+        }
+    } );
+ 
+    $('.datatable-Campaign').on('requestChild.dt', function(e, row) {
+        row.child(format(row.data())).show();
+    })
+ 
+    var table = $('.datatable-Campaign').DataTable( {
+        "ajax": $.fn.dataTable.pipeline( {
+            url: "{{ route('admin.campaigns.index') }}",
+            pages: 5 // number of pages to cache
+         } ),
+         "processing": true,
+        "serverSide": true,
+        "rowId": 'id',
+        "stateSave": true,
+        "columns": [
+            {
+                "className":      'dt-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
+            { "data": "placeholder","orderable":      false  },
+            { "data": "id" },
+            // { "data": "name" },
+            { "data": "email_subject" },
+            { "data": "sentDateTime" },
+            // { "data": "send_to" },
+            { "data": "actions" }
+        ],
+        "order": [[1, 'asc']]
+    } );
+} );
 
 </script>
 
--- resources/views/admin/unsubscribers/index.blade.php
+++ resources/views/admin/unsubscribers/index.blade.php
@@ -74,7 +74,7 @@
     aaSorting: [],
     ajax: "{{ route('admin.unsubscribers.index') }}",
     columns: [
-      { data: 'placeholder', name: 'placeholder' },
+      { data: 'placeholder', name: 'placeholder', orderable:false},
 { data: 'id', name: 'id' },
 { data: 'email', name: 'email' },
 // { data: 'actions', name: '{{ trans('global.actions') }}' }
