242bd815557729731915cd2e69a62b407f98f7b9
Phillip Madsen <58571584+ap-coder@users.noreply.github.com>
2022-02-03 13:39:26 -0700

    Update 2022_02_03_183922_add_fields_to_payment_methods.php

--- database/migrations/2022_02_03_183922_add_fields_to_payment_methods.php
+++ database/migrations/2022_02_03_183922_add_fields_to_payment_methods.php
@@ -14,19 +14,16 @@ class AddFieldsToPaymentMethods extends Migration
     public function up()
     {
         Schema::table('payment_methods', function (Blueprint $table) {
-            //
+            $table->longText('explanation')->nullable();
+            $table->boolean('name_select')->default(0)->nullable();
+            $table->boolean('company_select')->default(0)->nullable();
+            $table->boolean('account_num_select')->default(0)->nullable();
+            $table->boolean('routing_select')->default(0)->nullable();
+            $table->boolean('explanation_select')->default(0)->nullable();
+            $table->string('custom_email')->nullable();
+            $table->boolean('custom_email_select')->default(0)->nullable();
+            $table->boolean('swift_select')->default(0)->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('payment_methods', function (Blueprint $table) {
-            //
-        });
-    }
 }
