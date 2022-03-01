efb5924995d9131938f29107533649a256654389
Phillip Madsen <ap-coder@users.noreply.github.com>
2022-03-01 21:31:14 +0000

    Apply fixes from StyleCI

--- database/migrations/2022_02_17_174529_drop_fields_to_payment_methods_table.php
+++ database/migrations/2022_02_17_174529_drop_fields_to_payment_methods_table.php
@@ -14,7 +14,7 @@ class DropFieldsToPaymentMethodsTable extends Migration
     public function up()
     {
         Schema::table('payment_methods', function (Blueprint $table) {
-            $table->dropColumn(['w8_select', 'w9_select','w8','w9']);
+            $table->dropColumn(['w8_select', 'w9_select', 'w8', 'w9']);
         });
     }
 }
